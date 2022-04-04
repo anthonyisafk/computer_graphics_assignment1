742a9921a04baa3d81c44b6fc5ba895c0230b3ed
anthonyisafk <72876359+anthonyisafk@users.noreply.github.com>
2022-04-05 02:03:42 +0300

    Create report.jl

---
+++ report/report.jl
@@ -0,0 +1,296 @@
+### A Pluto.jl notebook ###
+# v0.17.4
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 50937853-1837-4c66-8a26-40ef551cdcaf
+using PlutoUI
+
+# ╔═╡ 96f316bf-3c58-48ef-b1e1-572c03a31446
+md"""
+# Γραφική με Υπολογιστές
+## Αριστοτέλειο Πανεπιστήμιο Θεσσαλονίκης - Τμήμα Ηλεκτρολόγων Μηχανικών Μηχανικών Υπολογιστών
+### 1η Εργασία Εξαμήνου: Πλήρωση Τριγώνων
+### Αντωνίου Αντώνιος - aantonii@ece.auth.gr - 9482 
+"""
+
+# ╔═╡ 5e1807a9-7987-4f0b-a7f7-27b2db2adbfd
+md"""
+## Σκοπός της εργασίας
+Στο πλαίσιο της πρώτης εργασίας του μαθήματος καλούμαστε να χρωματίσουμε μία εικόνα η οποία είναι χωρισμένη σε πολύγωνα (το συγκεκριμένο παραδοτέο πραγματεύεται την **ειδική περίπτωση των τριγώνων**, τα οποία είναι πάντα **κυρτά**). Για το χρωματισμό του αντικειμένου χρειαζόμαστε μόνο:
+* Τις θέσεις όλων των κορυφών
+* Το χρώμα αυτών (σε percentage RGB)
+* Έναν πίνακα με τριάδες των δεικτών των κορυφών από τις οποίες αποτελείται το κάθε τρίγωνο.
+Ο χρωματισμός του αντικειμένου γίνεται με δύο τρόπους (όρισμα `shade_t`):
+* *flat*: Ως χρώμα όλου του τριγώνου επιλέγονται οι μέσοι όροι των τιμών `R`, `G` και `Β` των κορυφών που το απαρτίζουν
+* *gouraud*: Το χρώμα στο τρίγωνο δεν είναι ενιαίο αλλά ένα **gradient** που εξαρτάται ξανά από τις κορυφές του τριγώνου. Πιο αναλυτική περιγραφή του αλγορίθμου παρακάτω.
+
+## Ανάλυση των βημάτων της επίλυσης
+### Οργάνωση των δεδομένων
+Τα δεδομένα του προβλήματος βρίσκονται στο αρχείο `hw1.npy`, στο οποίο είναι αποθηκευμένα ως ένα *dictionary* από *numpy arrays*. Γι' αυτό και αρχικοποιούμε το αρχείο δεδομένων μας και παίρνουμε τα arrays ως εξής:
+```python
+data = np.load("hw1.npy", allow_pickle=True)[()]
+verts2d = data['verts2d']
+vcolors = data['vcolors']
+faces = data['faces']
+depth = data['depth']
+```
+Τα δεδομένα αυτά ύστερα δίνονται στη συνάρτηση `render()`, εντός της οποίας γίνεται το preprocessing.
+```python
+def render(verts2d, faces, vcolors, depth, shade_t, M=512, N=512, refresh=False)
+```
+M και N είναι οι διαστάσεις της εικόνας και `refresh` είναι ένας *Boolean* που καθορίζει αν κατά την εκτέλεση του αλγορίθμου ο χρήστης θέλει να βλέπει την πορεία της πλήρωσης των τριγώνων του αντικειμένου σε ένα παράθυρο της openCV.
+
+### Preprocessing
+Μιας και χρησιμοποιούμε openCV για την εμφάνιση και αποθήκευση των εικόνων, οφείλουμε να μετετρέψουμε τα χρώματα από **RGB** σε **BGR**, standard με το οποίο συμφωνεί η βιβλιοθήκη.
+```python
+for i in range(len(vcolors)):
+	vcolors[i] = np.flip(vcolors[i])
+```
+Έπειτα γεμίζουμε έναν πίνακα **img** διάστασης `MxNx3` με μονάδες, που αντιστοιχούν σε λευκό χρώμα. Στον πίνακα αυτό, συμβουλευόμενοι τις συντεταγμένες **verts2d** και τα χρώματα **vcolors** των κορυφών, γράφουμε τα προϋπάρχοντα χρώματα.
+Το επόμενο βήμα είναι ο καθορισμός της σειράς με την οποία θα χρωματιστούν τα τρίγωνα της φωτογραφίας, μιας και απεικονίζουν ένα τρισδιάστατο αντικείμενο, που συνεπάγεται διαφορετικό βάθος για το καθένα. Το βάθος αυτό υπολογίζεται ως ο μέσος όρος των βαθών των κορυφών και εντάσσεται σε ένα array το οποίο ταξινομείται με φθίνουσα σειρά. Με βάση αυτή τη σειρά ξεκινάμε να χρωματίζουμε ένα-ένα τα τρίγωνα με τη χρήση της συνάρτησης:
+```python
+def shade_triangle(img, verts2d, vcolors, shade_t)
+```
+Στη `shade_triangle()` δίνουμε μόνο τα 3 *verts2d* και *vcolors* για τις κορυφές που διαμορφώνουν το τρίγωνο.
+
+### Η συνάρτηση shade_triangle()
+Εντός της συνάρτησης ελέγχεται αν το όρισμα `shade_t` είναι ίδιο με μία από τις δύο αποδεκτές τιμές *flat* και *gouraud* και γίνεται μία αρχική εκτίμηση των πλευρών, με τη συνάρτηση `find_edges(verts2d)`. Αυτή αναλαμβάνει να επιστρέψει τις πλευρές του τριγώνου ως ένα σύνολο από arrays:
+* **xkmax, xkmin, ykmax, ykmin**: Είναι οι μέγιστες και ελάχιστες συνιστώσες των συντεταγμένων της κάθε πλευράς
+* **max_owners και min_owners**: Δηλώνουν ποια κορυφή κατέχει τη την κάθε τιμή από τα xkmax, xkmin, ykmax και ykmin
+* **mi και bi**: Κάθε πλευρά αναπαριστάται ως μία ευθεία με εξίσωση $y=m\cdot x+b$. Για την κάθε παράμετρο ισχύει:
+    * $m=\frac{y_{start}-y_{end}}{x_{start}-x_{end}}$
+    * $b=y_{start}-m\cdot x_{start}$
+**Σημειώνουμε πως η *πλευρά i* αποτελείται από τις κορυφές *i και i+1* (ή τις 2 και 0, στην περίπτωση της τελευταίας πλευράς).**
+
+Στο τέλος, η συνάρτηση καλεί την υπορουτίνα `shade_triangle_flat()` ή `shade_triangle_gouraud()` ανάλογα την τιμή του `shade_t`.
+
+### Διαφορές "flat" και "gouraud"
+Και στις δύο περιπτώσεις γεμίζουμε το τρίγωνο **ανά scanline**. Σε κάθε iteration, αρα για κάθε $y_{s}$, βρίσκουμε τα σημεία τομής της ευθείας $y=y_{s}$ με τις ενεργές πλευρές, χρησιμοποιώντας τις παραμέτρους τους *m και b*. Οι ενεργές πλευρές έχουν βρεθεί για $y=y_{min}$ και ύστερα για κάθε y ενημερώνονται αναδρομικά σύμφωνα με τον αλγόριθμο των σημειώσεων, έχοντας στο νου μας την παραδοχή ότι το πολύγωνο που διαχειριζόμαστε είναι τρίγωνο (οπότε υποχρεωτικά κυρτό):
+```
+if (ymax == y):
+	exclude active edge from the list
+ 	introduce the edge that was left out in the list
+```
+Αφού βρεθούν τα σημεία τομής ακολουθούμε δύο διαφορετικές τακτικές:
+* Για `shade_t == "flat"` απλά γεμίζουμε με το χρώμα που είναι ο μέσος όρος των κορυφών του τριγώνου από το πρώτο σημείο τομής μέχρι το δεύτερο. Αυτό μπορούμε να το κάνουμε γιατί το τρίγωνο, ως **κυρτό πολύγωνο** έχει ένα **συνεχές διάστημα πλήρωσης** για κάθε scanline.
+* Για `shade_t == "gouraud"` πρώτα παρεμβάλλουμε γραμμικά τα σημεία τομής χρησιμοποιώντας τις κάθετες συνιστώσες των συντεταγμένων των ίδιων των σημείων και των κορυφών των πλευρών πάνω στις οποίες βρίσκονται. Έτσι έχουμε το χρώμα των σημείων των πλευρών του τριγώνου για το $y_{s}$. Ακολούθως, χρησιμοποιούμε γραμμική παρεμβολή στις οριζόντιες συνιστώσες των συντεταγμένων των σημείων του διαστήματος πλήρωσης, με άκρα, φυσικά, τα σημεία τομής.
+
+### Αποτελέσματα
+#### Flat
+**Ενδεικτικός χρόνος εκτέλεσης:**
+
+#### Gouraud
+**Ενδεικτικός χρόνος εκτέλεσης:**
+
+$(LocalResource("../image/flat.jpg"))
+
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+PlutoUI = "7f904dfe-b85e-4ff6-b463-dae2292396a8"
+
+[compat]
+PlutoUI = "~0.7.38"
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+[[AbstractPlutoDingetjes]]
+deps = ["Pkg"]
+git-tree-sha1 = "8eaf9f1b4921132a4cff3f36a1d9ba923b14a481"
+uuid = "6e696c72-6542-2067-7265-42206c756150"
+version = "1.1.4"
+
+[[ArgTools]]
+uuid = "0dad84c5-d112-42e6-8d28-ef12dabb789f"
+
+[[Artifacts]]
+uuid = "56f22d72-fd6d-98f1-02f0-08ddc0907c33"
+
+[[Base64]]
+uuid = "2a0f44e3-6c83-55bd-87e4-b1978d98bd5f"
+
+[[ColorTypes]]
+deps = ["FixedPointNumbers", "Random"]
+git-tree-sha1 = "024fe24d83e4a5bf5fc80501a314ce0d1aa35597"
+uuid = "3da002f7-5984-5a60-b8a6-cbb66c0b333f"
+version = "0.11.0"
+
+[[Dates]]
+deps = ["Printf"]
+uuid = "ade2ca70-3891-5945-98fb-dc099432e06a"
+
+[[Downloads]]
+deps = ["ArgTools", "LibCURL", "NetworkOptions"]
+uuid = "f43a241f-c20a-4ad4-852c-f6b1247861c6"
+
+[[FixedPointNumbers]]
+deps = ["Statistics"]
+git-tree-sha1 = "335bfdceacc84c5cdf16aadc768aa5ddfc5383cc"
+uuid = "53c48c17-4a7d-5ca2-90c5-79b7896eea93"
+version = "0.8.4"
+
+[[Hyperscript]]
+deps = ["Test"]
+git-tree-sha1 = "8d511d5b81240fc8e6802386302675bdf47737b9"
+uuid = "47d2ed2b-36de-50cf-bf87-49c2cf4b8b91"
+version = "0.0.4"
+
+[[HypertextLiteral]]
+git-tree-sha1 = "2b078b5a615c6c0396c77810d92ee8c6f470d238"
+uuid = "ac1192a8-f4b3-4bfe-ba22-af5b92cd3ab2"
+version = "0.9.3"
+
+[[IOCapture]]
+deps = ["Logging", "Random"]
+git-tree-sha1 = "f7be53659ab06ddc986428d3a9dcc95f6fa6705a"
+uuid = "b5f81e59-6552-4d32-b1f0-c071b021bf89"
+version = "0.2.2"
+
+[[InteractiveUtils]]
+deps = ["Markdown"]
+uuid = "b77e0a4c-d291-57a0-90e8-8db25a27a240"
+
+[[JSON]]
+deps = ["Dates", "Mmap", "Parsers", "Unicode"]
+git-tree-sha1 = "3c837543ddb02250ef42f4738347454f95079d4e"
+uuid = "682c06a0-de6a-54ab-a142-c8b1cf79cde6"
+version = "0.21.3"
+
+[[LibCURL]]
+deps = ["LibCURL_jll", "MozillaCACerts_jll"]
+uuid = "b27032c2-a3e7-50c8-80cd-2d36dbcbfd21"
+
+[[LibCURL_jll]]
+deps = ["Artifacts", "LibSSH2_jll", "Libdl", "MbedTLS_jll", "Zlib_jll", "nghttp2_jll"]
+uuid = "deac9b47-8bc7-5906-a0fe-35ac56dc84c0"
+
+[[LibGit2]]
+deps = ["Base64", "NetworkOptions", "Printf", "SHA"]
+uuid = "76f85450-5226-5b5a-8eaa-529ad045b433"
+
+[[LibSSH2_jll]]
+deps = ["Artifacts", "Libdl", "MbedTLS_jll"]
+uuid = "29816b5a-b9ab-546f-933c-edad1886dfa8"
+
+[[Libdl]]
+uuid = "8f399da3-3557-5675-b5ff-fb832c97cbdb"
+
+[[LinearAlgebra]]
+deps = ["Libdl"]
+uuid = "37e2e46d-f89d-539d-b4ee-838fcccc9c8e"
+
+[[Logging]]
+uuid = "56ddb016-857b-54e1-b83d-db4d58db5568"
+
+[[Markdown]]
+deps = ["Base64"]
+uuid = "d6f4376e-aef5-505a-96c1-9c027394607a"
+
+[[MbedTLS_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "c8ffd9c3-330d-5841-b78e-0817d7145fa1"
+
+[[Mmap]]
+uuid = "a63ad114-7e13-5084-954f-fe012c677804"
+
+[[MozillaCACerts_jll]]
+uuid = "14a3606d-f60d-562e-9121-12d972cd8159"
+
+[[NetworkOptions]]
+uuid = "ca575930-c2e3-43a9-ace4-1e988b2c1908"
+
+[[Parsers]]
+deps = ["Dates"]
+git-tree-sha1 = "621f4f3b4977325b9128d5fae7a8b4829a0c2222"
+uuid = "69de0a69-1ddd-5017-9359-2bf0b02dc9f0"
+version = "2.2.4"
+
+[[Pkg]]
+deps = ["Artifacts", "Dates", "Downloads", "LibGit2", "Libdl", "Logging", "Markdown", "Printf", "REPL", "Random", "SHA", "Serialization", "TOML", "Tar", "UUIDs", "p7zip_jll"]
+uuid = "44cfe95a-1eb2-52ea-b672-e2afdf69b78f"
+
+[[PlutoUI]]
+deps = ["AbstractPlutoDingetjes", "Base64", "ColorTypes", "Dates", "Hyperscript", "HypertextLiteral", "IOCapture", "InteractiveUtils", "JSON", "Logging", "Markdown", "Random", "Reexport", "UUIDs"]
+git-tree-sha1 = "670e559e5c8e191ded66fa9ea89c97f10376bb4c"
+uuid = "7f904dfe-b85e-4ff6-b463-dae2292396a8"
+version = "0.7.38"
+
+[[Printf]]
+deps = ["Unicode"]
+uuid = "de0858da-6303-5e67-8744-51eddeeeb8d7"
+
+[[REPL]]
+deps = ["InteractiveUtils", "Markdown", "Sockets", "Unicode"]
+uuid = "3fa0cd96-eef1-5676-8a61-b3b8758bbffb"
+
+[[Random]]
+deps = ["Serialization"]
+uuid = "9a3f8284-a2c9-5f02-9a11-845980a1fd5c"
+
+[[Reexport]]
+git-tree-sha1 = "45e428421666073eab6f2da5c9d310d99bb12f9b"
+uuid = "189a3867-3050-52da-a836-e630ba90ab69"
+version = "1.2.2"
+
+[[SHA]]
+uuid = "ea8e919c-243c-51af-8825-aaa63cd721ce"
+
+[[Serialization]]
+uuid = "9e88b42a-f829-5b0c-bbe9-9e923198166b"
+
+[[Sockets]]
+uuid = "6462fe0b-24de-5631-8697-dd941f90decc"
+
+[[SparseArrays]]
+deps = ["LinearAlgebra", "Random"]
+uuid = "2f01184e-e22b-5df5-ae63-d93ebab69eaf"
+
+[[Statistics]]
+deps = ["LinearAlgebra", "SparseArrays"]
+uuid = "10745b16-79ce-11e8-11f9-7d13ad32a3b2"
+
+[[TOML]]
+deps = ["Dates"]
+uuid = "fa267f1f-6049-4f14-aa54-33bafae1ed76"
+
+[[Tar]]
+deps = ["ArgTools", "SHA"]
+uuid = "a4e569a6-e804-4fa4-b0f3-eef7a1d5b13e"
+
+[[Test]]
+deps = ["InteractiveUtils", "Logging", "Random", "Serialization"]
+uuid = "8dfed614-e22c-5e08-85e1-65c5234f0b40"
+
+[[UUIDs]]
+deps = ["Random", "SHA"]
+uuid = "cf7118a7-6976-5b1a-9a39-7adc72f591a4"
+
+[[Unicode]]
+uuid = "4ec0a83e-493e-50e2-b9ac-8f72acf5a8f5"
+
+[[Zlib_jll]]
+deps = ["Libdl"]
+uuid = "83775a58-1f1d-513f-b197-d71354ab007a"
+
+[[nghttp2_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "8e850ede-7688-5339-a07c-302acd2aaf8d"
+
+[[p7zip_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "3f19e933-33d8-53b3-aaab-bd5110c3b7a0"
+"""
+
+# ╔═╡ Cell order:
+# ╟─50937853-1837-4c66-8a26-40ef551cdcaf
+# ╟─96f316bf-3c58-48ef-b1e1-572c03a31446
+# ╠═5e1807a9-7987-4f0b-a7f7-27b2db2adbfd
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
